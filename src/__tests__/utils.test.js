6e7940ffc2a2b8593d3e9ab8e4d83e32387f032e
Zacharia Ndinguri <zacthuku@gmail.com>
2025-04-22 12:40:23 +0300

    tdd lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -2,8 +2,15 @@ import { currentAgeForBirthYear } from "../utils";
 
 describe("currentAgeForBirthYear", () => {
   it("returns the age of a person based on the year of birth", () => {
+    // Arrange
+    const currentYear = new Date().getFullYear();
     const birthYear = 1984;
+    const answer = currentYear - birthYear;
+
+    // Act
     const ageOfPerson = currentAgeForBirthYear(birthYear);
-    expect(ageOfPerson).toBe(38);
+
+    // Assert
+    expect(ageOfPerson).toEqual(answer);
   });
 });
